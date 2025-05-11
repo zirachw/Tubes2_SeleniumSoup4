a9e5b9f7b0ee0d6b323449fb52ae36ddd663d523
mlqmn <13523044@std.stei.itb.ac.id>
2025-05-11 19:17:54 +0700

    refactor: change dfs to search

--- src/backend/cmd/server/main.go
+++ src/backend/cmd/server/main.go
@@ -9,6 +9,7 @@ import (
 
 	// "encoding/json"
 	"github.com/zirachw/Tubes2_SeleniumSoup4/internal/scraper"
+	"github.com/zirachw/Tubes2_SeleniumSoup4/internal/search"
 )
 
 func makeRecipeHandler(data map[string]scraper.ElementData) func(w http.ResponseWriter, r *http.Request) {
@@ -44,15 +45,28 @@ func makeRecipeHandler(data map[string]scraper.ElementData) func(w http.Response
 		}
 
 		// 4) run the right search
-		//switch algo {
-		//case "bfs":
+		switch algo {
+		// case "bfs":
 		//    recipes = BFS(graph, elem, limit)
-		//case "dfs":
-		//    recipes = DFS(graph, elem, limit)
-		//default:
-		//    http.Error(w, "algorithm must be bfs or dfs", http.StatusBadRequest)
-		//    return
-		//}
+		case "dfs":
+			var memo = &map[string]search.Tree{}
+			var root = &search.Root{
+				Value:     elem,
+				Children:  nil,
+				Up:        nil,
+				NSolution: 0,
+				NVisited:  0}
+			var tree search.Tree = root
+			search.DFSSearch(data, elem, limit, *memo, &tree)
+			var res, err = search.TreeToJSON(tree)
+			if err == nil {
+				w.Header().Set("Content-Type", "application/json")
+				w.Write(res)
+			}
+		default:
+			http.Error(w, "algorithm must be bfs or dfs", http.StatusBadRequest)
+			return
+		}
 
 		// 5) write JSON response
 
